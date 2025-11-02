02bdcf59aca34f8e1f5d8c4bb73593b40fb1b1cb
GamerPerson57 <amlorden21@gmail.com>
2025-11-02 13:07:12 -0500

    steps 8-10 and finished

--- code.js
+++ code.js
@@ -3,25 +3,47 @@ let pencil = canvas.getContext("2d"); // This gives you the drawing context, lik
 
 import { Star } from "./star.js";
 
+//draw stars
 let mySpecialStar = new Star(canvas, pencil);
 mySpecialStar.draw();
 
+let mySpecialStar2 = new Star(canvas, pencil);
+mySpecialStar2.draw();
+
+let mySpecialStar3 = new Star(canvas, pencil);
+mySpecialStar3.draw();
+
+
+// creates first three stars
 let stars = [
-    mySpecialStar
+    mySpecialStar,
+    mySpecialStar2,
+    mySpecialStar3,
 ];
 
+// adds a 100 stars
+for(let i = 0; i <= 100; i++) {
+    let multipleStars = new Star(canvas,pencil);
+    stars.push(multipleStars)
+    multipleStars.draw();
+}
+
+//Game loop
 function gameLoop() {
-    pencil.clearRect(0, 0, canvas.width, canvas.height);
     //erase canvas
+    pencil.clearRect(0, 0, canvas.width, canvas.height);
     //draw background
-    //draw stars
+    
     //move stars
     for(let i = 0; i < stars.length; i++) {
         stars[i].move();
         stars[i].draw();
-    }
-    //recycle stars/restart stars
 
+         //recycle stars/restart stars
+        if (stars[i].x >= canvas.width) {
+        stars[i].x = 0;
+    }
+}
 
 
 }
--- star.js
+++ star.js
@@ -5,16 +5,17 @@ export class Star {
     y = 50;
     pencil;
 
-    constructor(canvas,pencil) {
+    constructor(canvas, pencil, size) {
         this.pencil = pencil;
         this.x = canvas.width * Math.random();
         this.y = canvas.height * Math.random();
+        this.size = size || Math.random() * 4 + 1; // defaults the size between 1 and 4
     }
 
     draw(pencil) {
         this.pencil.beginPath();
-        this.pencil.arc(this.x, this.y, 40, 0, 2 * Math.PI);
-        this.pencil.fillStyle = "red";
+        this.pencil.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+        this.pencil.fillStyle = "white";
         this.pencil.fill();
         this.pencil.closePath();
     }
